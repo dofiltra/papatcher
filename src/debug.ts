8d479c14cb5a791498cd7ee841976b51c25dc57d
memanot <menemty@gmail.com>
2021-09-19 12:47:00 +0300

    getLastVersion

--- src/debug.ts
+++ src/debug.ts
@@ -1,3 +1,15 @@
+import { Papatcher } from '.'
+
 const debug = async () => {
-    // 
+  const papatcher = new Papatcher({
+    appPath: '../',
+    apiUrl: 'https://dofiltra.com/api/doproxificator?getVersion=true'
+  })
+  const currVersion = await papatcher.getCurrentVersion()
+
+  const testVersion = '2.0.2'
+//   console.log(currVersion, testVersion)
+//   console.log('Need new version', currVersion < testVersion)
 }
+
+debug()
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import { getPackageJson } from 'esm-requirer'
+import { sleep } from 'time-helpers'
 
 type TPapatcherSettings = {
   appPath: string
+  apiUrl: string
+}
+
+type TLastVersionOpts = {
+  tryLimit?: number
+  tryIndex?: number
 }
 
 class Papatcher {
@@ -16,6 +23,34 @@ class Papatcher {
   async getCurrentVersion() {
     const pack = await getPackageJson(this._settings.appPath)
     return pack.version
+  }
+
+  async getLastVersion(opts?: TLastVersionOpts): Promise<any> {
+    const { tryIndex = 0, tryLimit = 1 } = { ...opts }
+
+    if (tryIndex > tryLimit) {
+      return null
+    }
+
+    const { apiUrl } = this._settings
+    const resp = await fetch(apiUrl, {})
+
+    if (!resp?.ok) {
+      await sleep(1e3)
+      return await this.getLastVersion({
+        ...opts,
+        tryIndex: tryIndex + 1
+      })
+    }
+
+    return await resp.json()
+  }
+
+  async needUpdate() {
+    const currVersion = await this.getCurrentVersion()
+    const lastVersion = await this.getLastVersion()
+
+    return currVersion < lastVersion
   }
 }
 
