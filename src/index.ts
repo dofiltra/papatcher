0b76c0137b642d1459e6cd956afdbc634ce65a30
memanot <menemty@gmail.com>
2021-09-19 14:22:50 +0300

    unzip

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
-import fs from 'fs'
-import path from 'path'
 import fetch from 'node-fetch'
 import { getPackageJson } from 'esm-requirer'
 import { sleep } from 'time-helpers'
-import DecompressZip from 'decompress-zip'
+import admZip from 'adm-zip'
 
 type TPapatcherSettings = {
   appPath: string
@@ -14,6 +12,7 @@ type TPapatcherSettings = {
 type TLastVersionOpts = {
   tryLimit?: number
   tryIndex?: number
+  tryDelay?: number
 }
 
 class Papatcher {
@@ -31,7 +30,7 @@ class Papatcher {
   }
 
   async getLastVersion(opts?: TLastVersionOpts): Promise<any> {
-    const { tryIndex = 0, tryLimit = 1 } = { ...opts }
+    const { tryIndex = 0, tryLimit = 1, tryDelay = 1e3 } = { ...opts }
 
     if (tryIndex > tryLimit) {
       return null
@@ -41,7 +40,7 @@ class Papatcher {
     const resp = await fetch(apiUrl, {})
 
     if (!resp?.ok) {
-      await sleep(1e3)
+      await sleep(tryDelay)
       return await this.getLastVersion({
         ...opts,
         tryIndex: tryIndex + 1
@@ -59,7 +58,7 @@ class Papatcher {
   }
 
   async update(force = false) {
-    const { appPath, filename } = this._settings
+    const { appPath } = this._settings
     const { needUpdate, downloadUrl } = await this.getInfo()
 
     if (!force && !needUpdate) {
@@ -68,40 +67,13 @@ class Papatcher {
 
     try {
       const resp = await fetch(downloadUrl)
-      const filepath = path.join(appPath, filename)
-      const dest = fs.createWriteStream(filepath)
-      console.log(filepath)
+      const zip = new admZip(await resp.buffer())
+      zip.extractAllTo(appPath, true)
 
-      resp.body?.pipe(dest)
-      await sleep(10e3)
-      this.unzip(filepath)
-
-      return { result: !!resp.body }
+      return { result: resp.ok }
     } catch (error) {
       return { error }
     }
-  }
-
-  private unzip(filepath: string) {
-    const unzipper = new DecompressZip(filepath)
-    unzipper.on('error', function (err: any) {
-      console.log('Caught an error')
-    })
-
-    unzipper.on('extract', function (log: any) {
-      console.log('Finished extracting')
-    })
-
-    unzipper.on('progress', function (fileIndex: number, fileCount: number) {
-      console.log('Extracted file ' + (fileIndex + 1) + ' of ' + fileCount)
-    })
-
-    unzipper.extract({
-      path: filepath,
-      filter: function (file: File) {
-        return file.type !== 'SymbolicLink'
-      }
-    })
   }
 }
 
